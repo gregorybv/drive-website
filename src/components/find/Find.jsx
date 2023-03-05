7c7f2673217d6e53a0d97c46a55357e8de900640
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-05 11:08:02 +0300

    add swiper to project

--- src/components/find/Find.jsx
+++ src/components/find/Find.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from "./Find.module.css"
 
+import {Navigation, Pagination, Scrollbar, A11y} from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react";
+
 const Find = () => {
   return (
     <div>
